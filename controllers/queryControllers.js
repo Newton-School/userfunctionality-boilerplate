61a32cf0f3331b57aaaf8eaa806e15806dc1d16e
Shahista Shaikh <83348007+shahista786@users.noreply.github.com>
2023-12-17 17:17:28 +0530

    Update queryControllers.js

--- controllers/queryControllers.js
+++ controllers/queryControllers.js
@@ -1,15 +1,42 @@
-//Before implementing queryController.js plz refer to userModel first
 
 const User = require('../models/userModel');
 
 const filterQueries = async (req, res) => {
   try {
-    //Write your code here for sorting,pagination and searching
-    //1) For sorting sort salary from ascending to descending order
-    //2) For Pagination set limit 5 as a default limit and default page is 1
-    //3) Implement serching on 'first_name' and 'last_name'
-    // Formulae to implementing pagination: (page - 1) * limit
-    // For Sorting use    .sort('salary')
+    // Extract query parameters
+    const { first_name, last_name, sort, page = 1, limit = 5 } = req.query;
+
+    // Build query object for searching
+    const queryObject = {};
+    if (first_name) {
+      queryObject.first_name = { $regex: first_name, $options: 'i' };
+    }
+    if (last_name) {
+      queryObject.last_name = { $regex: last_name, $options: 'i' };
+    }
+
+    // Build sort object for sorting
+    const sortObject = {};
+    if (sort) {
+      const sortOrder = sort.startsWith('-') ? -1 : 1;
+      const sortBy = sort.replace(/^-/, '');
+      sortObject[sortBy] = sortOrder;
+    } else {
+      // Default sorting by age in ascending order
+      sortObject.age = 1;
+    }
+
+    // Calculate skip value for pagination
+    const skip = (page - 1) * limit;
+
+    // Execute the query with pagination, searching, and sorting
+    const users = await User.find(queryObject)
+      .sort(sortObject)
+      .skip(skip)
+      .limit(parseInt(limit));
+
+    // Return the results
+    res.status(200).json(users);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Internal Server Error' });
